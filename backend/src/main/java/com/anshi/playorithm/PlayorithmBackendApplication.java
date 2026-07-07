cace05c0a31ec54d63fc01c3eb82fe471ef4d0d6
Anshika Srivastava <srivastavaanshika510@gmail.com>
2026-07-07 16:10:08 +0530

    Initial commit

---
+++ backend/src/main/java/com/anshi/playorithm/PlayorithmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anshi.playorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayorithmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayorithmBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/anshi/playorithm/PlayorithmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshi.playorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayorithmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
